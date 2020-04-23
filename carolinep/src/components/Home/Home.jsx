6574ef9158258e33c7b04aad5edca92b6a4e8dad
Andrew J Kim <andrewk745@gmail.com>
2020-04-22 23:21:06 -0400

    made render second bullets and third bullets

--- carolinep/src/components/Home/Home.jsx
+++ carolinep/src/components/Home/Home.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import pianoHome from '../../images/piano.jpg'
 import './styles/Home.scss'
 import EmailIcon from '@material-ui/icons/Email';
-import { bullets } from '../../data'
+import { bullets, secondBullets, thirdBullets } from '../../data'
 
 export default class Home extends Component {
     constructor(props) {
@@ -15,20 +15,65 @@ export default class Home extends Component {
 
 
 
-        renderBullets = () => {
-            return (
-                <>
-                    {bullets.map(bullet => {
-                        return (
-                            <div class='is-flex'>
-                                <img src={bullet.src}/>
-                                <p>{bullet.bullet}</p>
+    renderBullets = () => {
+        return (
+            <>
+                {bullets.map(bullet => {
+                    return (
+                        <div class='columns is-flex'>
+                            <div class='column'>
+                                <img src={bullet.src} />
                             </div>
-                        )
-                    })}
-                </>
-            )
-        }
+                            <div class='column is-three-quarters'>
+                                <p class='is-size-5 is-size-7-mobile'>{bullet.bullet}</p>
+                            </div>
+                        </div>
+                    )
+                })}
+            </>
+        )
+    }
+
+    renderBullets2 = () => {
+        return (
+            <>
+                {secondBullets.map(bullet => {
+                    return (
+                        <div class='columns is-flex'>
+                            <div class='column'>
+                                <img src={bullet.src} />
+                            </div>
+                            <div class='column is-three-quarters'>
+                                <p class='is-size-5 is-size-7-mobile'>{bullet.bullet}</p>
+                            </div>
+                        </div>
+                    )
+                })}
+            </>
+        )
+    }
+
+    renderBullets3 = () => {
+        return (
+            <>
+                {thirdBullets.map(bullet => {
+                    return (
+                        <div class='columns is-flex'>
+                            <div class='column'>
+                                <img src={bullet.src}/>
+                            </div>
+                            <div class='column is-three-quarters'>
+                            <p class='is-size-5 is-size-7-mobile'>{bullet.bullet}</p>
+                            </div>   
+                        </div>
+                    )
+                })}
+            </>
+        )
+    }
+
+
+
 
 
 
@@ -44,15 +89,19 @@ export default class Home extends Component {
                             <EmailIcon
                                 fontSize="large"
                             />
-                            <p class='is-size-5'>EMAIL WITH ANY QUESTIONS OR TO SET UP YOUR TRIAL LESSONS</p>
+                            <p class='is-size-5 is-size-7-mobile'>EMAIL WITH ANY QUESTIONS OR TO SET UP YOUR TRIAL LESSONS</p>
                         </div>
                     </div>
                     <div class="column">
                         <h1 class="title">Piano Lessons</h1>
                         <div class="box">
                             <div class="media-content">
-                                <h3 class="title">Best for students who</h3>
+                                <h3 class="title is-size-6-mobile">Best for students who</h3>
                                 {this.renderBullets()}
+                                <h3 class="title is-size-6-mobile">Prepare For</h3>
+                                {this.renderBullets2()}
+                                <h3 class="title is-size-6-mobile">Locations</h3>
+                                {this.renderBullets3()}
                             </div>
                         </div>
                     </div>
