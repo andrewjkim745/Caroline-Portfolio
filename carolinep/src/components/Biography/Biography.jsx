680782493aacae0e809eef4996b62bfb567b5033
Andrew J Kim <andrewk745@gmail.com>
2020-04-30 00:00:13 -0400

    fixed some image styling

--- carolinep/src/components/Biography/Biography.jsx
+++ carolinep/src/components/Biography/Biography.jsx
@@ -14,13 +14,15 @@ export const BiographyHero = (props) => {
         <>
         <div class="columns borderBottom no-margin-bot">
             <div class="column has-padding-small carolineDiv">
-                <Zoom duration={4000} delay={2000}>
-                    <figure class="image is-3by3">
-                        <img class="caroline2" src={caroline2} />
+                
+                    <figure class="image is-3by3 caroline2">
+                    <Zoom duration={4000} delay={1500}>
+                        <img src={caroline2} />
+                    </Zoom>
                     </figure>
-                </Zoom>
+                
             </div>
-            <Fade right duration={4000} delay={2000}>
+            <Fade right duration={4000} delay={1500}>
             <div class="column carolineRightDiv"> 
             
                 <div class="container has-text-centered has-padding-small">
