098fd518647b7b87d562601c046122c38a39ff36
Andrew J Kim <andrewk745@gmail.com>
2020-05-01 09:03:18 -0400

    added slide effect

--- carolinep/src/components/Credentials/Credentials.jsx
+++ carolinep/src/components/Credentials/Credentials.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ysm from '../../images/ysm.jpg'
 import juilliard2 from '../../images/juilliard2.jpg'
+import Slide from 'react-reveal/Slide'
 
 
 
@@ -11,14 +12,18 @@ export const Credentials = (props) => {
     return (
         <div class="columns">
             <div class="column has-background-primary">
-                <figure class="image is-1by1">
-                    <img src={ysm}/>
-                </figure>
+                <Slide left>
+                    <figure class="image is-1by1">
+                        <img src={ysm} />
+                    </figure>
+                </Slide>
             </div>
             <div class="column has-background-primary">
-                <figure class="image is-1by1">
-                    <img src={juilliard2}/>
-                </figure>
+                <Slide right>
+                    <figure class="image is-1by1">
+                        <img src={juilliard2} />
+                    </figure>
+                </Slide>
             </div>
         </div>
     )
