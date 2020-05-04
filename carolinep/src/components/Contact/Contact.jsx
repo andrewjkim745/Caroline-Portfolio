0ca2b2e99397559bc19027535bce665267a99ce9
Andrew J Kim <andrewk745@gmail.com>
2020-05-04 07:46:18 -0400

    set up contact form handlechanges

--- carolinep/src/components/Contact/Contact.jsx
+++ carolinep/src/components/Contact/Contact.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import emailjs from 'emailjs-com'
 import { ContactForm } from './ContactForm'
-
+import pianoart from '../../images/pianoart.JPG'
 
 
 
@@ -13,7 +13,8 @@ export default class Contact extends React.Component {
         this.state = {
             name: '',
             email: '',
-            message: ''
+            message: '',
+            error: false,
         }
     }
 
@@ -23,15 +24,45 @@ export default class Contact extends React.Component {
         this.setState({
             [e.target.name] : e.target.value
         })
+
+        console.log(this.state.email)
+
+        if (this.state.email.includes("@")) {
+            this.setState({
+                error: false
+            })
+            
+        } else {
+            this.setState({
+                error: true
+            })
+        }
     }
 
 
 
     render() {
         return (
-            <div class="columns">
-                <ContactForm/>
+            <>
+            <div class="container has-text-centered has-padding">
+                <h1 class="title is-size-1">Contact</h1>
             </div>
+            <div class="columns">
+                <ContactForm
+                onChange={this.handleChange}
+                emailClass={this.state.error === true ? "input is-danger" : "input"}
+                nameClass={this.state.name.length > 1 ? "input" : "input is-danger"}
+                name={this.state.name}
+                email={this.state.email}
+                message={this.state.message}
+                />
+                <div class="column has-padding-large">
+                    <figure class="image is-1by1 ">
+                        <img src={pianoart}/>
+                    </figure>
+                </div>
+            </div>
+            </>
         )
     }
 }
--- carolinep/src/components/Contact/ContactForm.jsx
+++ carolinep/src/components/Contact/ContactForm.jsx
@@ -7,34 +7,43 @@ import React from 'react'
 
 export const ContactForm = (props) => {
     return (
-        <div class="column has-padding">
+        <div class="column has-padding-large">
             <div class="field">
                 <label class="label">Name</label>
                 <div class="control">
-                    <input class="input" type="text" placeholder="Text input" />
+                    <input name='name' value={props.name} onChange={props.onChange} class={props.nameClass} type="text" placeholder="Name" />
                 </div>
+                {/* {this.props.renderNameError} */}
             </div>
             <div class="field">
                 <label class="label">Email</label>
-                <div class="control has-icons-left has-icons-right">
-                    <input class="input is-danger" type="email" placeholder="Email input" value="hello@" />
-                    <span class="icon is-small is-left">
-                        <i class="fas fa-envelope"></i>
-                    </span>
-                    <span class="icon is-small is-right">
-                        <i class="fas fa-exclamation-triangle"></i>
-                    </span>
+                <div class="control">
+                    <input name='email' value={props.email} onChange={props.onChange} class={props.emailClass} type="email" placeholder="Email" />
                 </div>
-                <p class="help is-danger">This email is invalid</p>
+                {/* {this.props.renderEmailError} */}
+            </div>
+            <div class="field">
+            <label class="label">Subject</label>
+            <div class="control">
+                <div class="select">
+                <select>
+                    <option>Lessons</option>
+                    <option>Personal</option>
+                </select>
+                </div>
+            </div>
             </div>
             <div class="field">
                 <label class="label">Message</label>
                 <div class="control">
-                    <textarea class="textarea" placeholder="Textarea"></textarea>
+                    <textarea name='message' value={props.message} onChange={props.onChange} class="textarea" placeholder="Textarea"></textarea>
                 </div>
             </div>
+            <div class="field is-grouped">
+            <div class="control">
+                <button class="button is-link">Submit</button>
+            </div>
+            </div>
         </div>
-
-
     )
 }
